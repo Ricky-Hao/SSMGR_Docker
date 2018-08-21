f569467e6357c9908b33e93956ce0b7e8ae206fb
Hao <a471558277@gmail.com>
2018-08-21 15:15:42 +0800

    Update docker_run.sh

--- docker_run.sh
+++ docker_run.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
-docker pull rickyhao/ssmgr_docker:0.19.2
+docker pull rickyhao/ssmgr_docker:0.17.14
 docker run --rm -d --name ssmgr --mount type=bind,source=/home/ricky/.ssmgr,target=/root/.ssmgr -p 8888:8888 rickyhao/ssmgr_docker:latest
